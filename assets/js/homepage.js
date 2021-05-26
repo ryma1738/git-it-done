2c49ffbdca94523410ae30d76bece49a6f3030cd
ryma1738 <51891293+ryma1738@users.noreply.github.com>
2021-05-26 12:15:26 -0600

    Added git feach comand

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,6 +1,12 @@
 
-function getUserRepos() {
-    console.log("function was called");
+function getUserRepos(user) {
+    var apiUrl = "https://api.github.com/users/" + user + "/repos";
+
+   fetch(apiUrl).then(function(responce) {
+       responce.json().then(function(data) {
+        console.log(data);
+       });
+   });
 }
 
-getUserRepos();
+getUserRepos("ryma1738");
