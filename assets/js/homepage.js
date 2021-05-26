2bf135426bcffaf4eb9901a923394945aff81784
ryma1738 <51891293+ryma1738@users.noreply.github.com>
2021-05-26 13:33:06 -0600

    added basic functionality to program

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -3,10 +3,65 @@ function getUserRepos(user) {
     var apiUrl = "https://api.github.com/users/" + user + "/repos";
 
    fetch(apiUrl).then(function(responce) {
-       responce.json().then(function(data) {
-        console.log(data);
-       });
+       if (responce.ok) {
+        responce.json().then(function(data) {
+            displayRepos(data, user);
+            console.log(data)
+           });
+       }
+       else {
+           alert("ERROR: GitHub User Not Found! Please enter a diffrent username!")
+       }
+       
    });
 }
 
-getUserRepos("ryma1738");
+function displayRepos(repos, searchTerm) {
+    if (repos.lenth === 0) {
+        $("#repos-container").text("No Repositories Found For User!")
+    }
+    $("#repos-container").text("");
+    $("#repo-search-term").text(searchTerm);
+
+    for (var i = 0; i < repos.length; i++) {
+        var repoName = repos[i].owner.login + "/" + repos[i].name;
+
+        var divItem = document.createElement("div");
+        divItem.classList = "list-item flex-row justify-space-between align-center"
+
+        var spanItem = document.createElement("span");
+        spanItem.textContent = repoName;
+
+        divItem.appendChild(spanItem);
+
+        var spanItemStatus = document.createElement("span");
+        spanItemStatus.classList = "flex-roiw align-center"
+
+        if (repos[i].open_issues_count > 0) {
+            spanItemStatus.innerHTML = "<i class='fas fa-times status-icon icon-danger'></i>" + repos[i].open_issues_count + " issue(s)"
+        }
+        else {
+            spanItemStatus.innerHTML = "<i class='fas fa-check-square status-icon icon-success'></i>";
+        }
+
+        divItem.appendChild(spanItemStatus);
+        var container = document.querySelector("#repos-container");
+        container.appendChild(divItem)
+    }
+}
+
+$("#user-form").on("submit", function(event){
+    event.preventDefault();
+    var user = $("#username").val();
+    if (user) {
+        getUserRepos(user);
+        $("#username").val("");
+    }
+    else {
+        alert("Please enter a GitHub username!")
+    }
+    
+});
+
+
+
